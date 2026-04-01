497eee9dd5bfc261e56213f7cb2b05583d008ef7
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-01 05:42:12 -0300

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,13 +1,26 @@
-# 🔐 CONFIGURACIÓN GENERAL
+# 🔐 CONFIGURACIÓN GENERAL MPSERVER 2026
 
+# --- TELEGRAM (BOT FATHER) ---
 TOKEN = "8367475601:AAED4j1-Y034_5ZdrtjlzYs7VJSLpEINNJo"
-
 ADMIN_ID = 8460418714
 
-# 🌐 PANEL WEB
+# --- ACCESO AL PANEL WEB (LOGIN) ---
+# Estos son los datos que te pedirá la pantalla de Login
+WEB_USER = "admin"
+WEB_PASS = "noa2026"  # Cambia esto por tu contraseña segura
+
+# --- INSTAGRAM (INSTAGRAPI) ---
+IG_USER = "tu_usuario_ig"
+IG_PASS = "tu_password_ig"
+
+# --- TIKTOK (TIKTOKAPI) ---
+TT_USER = "@tu_usuario_tt"
+
+# --- REDES Y HOSTING ---
 WEB_HOST = "0.0.0.0"
 WEB_PORT = 5000
 
-# 📂 ARCHIVOS
+# --- RUTAS DE ARCHIVOS ---
 LOG_FILE = "logs.txt"
 QUEUE_FILE = "queue.json"
+SESSION_IG = "session_ig.json" # Para no loguear cada vez y evitar baneos
